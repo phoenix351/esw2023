b75c9a18145d3face03ead64ac0d6f62848feb2b
phoenix351 <49157772+phoenix351@users.noreply.github.com>
2023-05-26 08:10:28 +0800

    add peternakan

---
+++ resources/views/modals/ruta-tab-peternakan.blade.php
@@ -0,0 +1,192 @@
+{{-- Tabel A --}}
+<div class="overflow-x-auto mt-4">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-4 py-3">
+                    <span class="sr-only">Actions</span>
+                </th>
+                <th scope="col" class="px-4 py-3 text-center">id</th>
+                <th scope="col" class="px-4 py-3 text-center">r310</th>
+                <th scope="col" class="px-4 py-3 text-center">r311</th>
+                <th scope="col" class="px-4 py-3 text-center">r312</th>
+                <th scope="col" class="px-4 py-3 text-center">r313</th>
+                <th scope="col" class="px-4 py-3 text-center">r314</th>
+                <th scope="col" class="px-4 py-3 text-center">r315</th>
+                <th scope="col" class="px-4 py-3 text-center">r316</th>
+                <th scope="col" class="px-4 py-3 text-center">r317</th>
+                <th scope="col" class="px-4 py-3 text-center">r318</th>
+                <th scope="col" class="px-4 py-3 text-center">r319</th>
+                <th scope="col" class="px-4 py-3 text-center">r320</th>
+                <th scope="col" class="px-4 py-3 text-center">r321</th>
+                <th scope="col" class="px-4 py-3 text-center">r322</th>
+                <th scope="col" class="px-4 py-3 text-center">r323</th>
+                <th scope="col" class="px-4 py-3 text-center">prov</th>
+                <th scope="col" class="px-4 py-3 text-center">kabkot</th>
+                <th scope="col" class="px-4 py-3 text-center">kec</th>
+                <th scope="col" class="px-4 py-3 text-center">desa</th>
+            </tr>
+        </thead>
+        <tbody id="peternakan-a-body">
+
+        </tbody>
+        </form>
+
+    </table>
+</div>
+<nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+    aria-label="Table navigation">
+    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+        Showing
+        <span id="peternakan-a-showing" class="font-semibold text-gray-900 dark:text-white">?</span>
+        of
+        <span id="peternakan-a-total" class="font-semibold text-gray-900 dark:text-white">?</span>
+    </span>
+    <ul id="peternakan-a-pagination" class="inline-flex items-stretch -space-x-px">
+
+    </ul>
+</nav>
+{{-- Tabel B --}}
+<div class="overflow-x-auto mt-4">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-4 py-3">
+                    <span class="sr-only">Actions</span>
+                </th>
+                <th scope="col" class="px-4 py-3 text-center">id</th>
+                <th scope="col" class="px-4 py-3 text-center">r310</th>
+                <th scope="col" class="px-4 py-3 text-center">r311</th>
+                <th scope="col" class="px-4 py-3 text-center">r312</th>
+                <th scope="col" class="px-4 py-3 text-center">r313</th>
+                <th scope="col" class="px-4 py-3 text-center">r314</th>
+                <th scope="col" class="px-4 py-3 text-center">r315</th>
+                <th scope="col" class="px-4 py-3 text-center">r316</th>
+                <th scope="col" class="px-4 py-3 text-center">r317</th>
+                <th scope="col" class="px-4 py-3 text-center">r318</th>
+                <th scope="col" class="px-4 py-3 text-center">r319</th>
+                <th scope="col" class="px-4 py-3 text-center">r320</th>
+                <th scope="col" class="px-4 py-3 text-center">r321</th>
+                <th scope="col" class="px-4 py-3 text-center">r322</th>
+                <th scope="col" class="px-4 py-3 text-center">r323</th>
+                <th scope="col" class="px-4 py-3 text-center">prov</th>
+                <th scope="col" class="px-4 py-3 text-center">kabkot</th>
+                <th scope="col" class="px-4 py-3 text-center">kec</th>
+                <th scope="col" class="px-4 py-3 text-center">desa</th>
+            </tr>
+        </thead>
+        <tbody id="peternakan-b-body">
+
+        </tbody>
+        </form>
+
+    </table>
+</div>
+<nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+    aria-label="Table navigation">
+    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+        Showing
+        <span id="peternakan-b-showing" class="font-semibold text-gray-900 dark:text-white">?</span>
+        of
+        <span id="peternakan-b-total" class="font-semibold text-gray-900 dark:text-white">?</span>
+    </span>
+    <ul id="peternakan-b-pagination" class="inline-flex items-stretch -space-x-px">
+
+    </ul>
+</nav>
+{{-- Tabel C --}}
+<div class="overflow-x-auto mt-4">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-4 py-3">
+                    <span class="sr-only">Actions</span>
+                </th>
+                <th scope="col" class="px-4 py-3 text-center">id</th>
+                <th scope="col" class="px-4 py-3 text-center">r310</th>
+                <th scope="col" class="px-4 py-3 text-center">r311</th>
+                <th scope="col" class="px-4 py-3 text-center">r312</th>
+                <th scope="col" class="px-4 py-3 text-center">r313</th>
+                <th scope="col" class="px-4 py-3 text-center">r314</th>
+                <th scope="col" class="px-4 py-3 text-center">r315</th>
+                <th scope="col" class="px-4 py-3 text-center">r316</th>
+                <th scope="col" class="px-4 py-3 text-center">r317</th>
+                <th scope="col" class="px-4 py-3 text-center">r318</th>
+                <th scope="col" class="px-4 py-3 text-center">r319</th>
+                <th scope="col" class="px-4 py-3 text-center">r320</th>
+                <th scope="col" class="px-4 py-3 text-center">r321</th>
+                <th scope="col" class="px-4 py-3 text-center">r322</th>
+                <th scope="col" class="px-4 py-3 text-center">r323</th>
+                <th scope="col" class="px-4 py-3 text-center">prov</th>
+                <th scope="col" class="px-4 py-3 text-center">kabkot</th>
+                <th scope="col" class="px-4 py-3 text-center">kec</th>
+                <th scope="col" class="px-4 py-3 text-center">desa</th>
+            </tr>
+        </thead>
+        <tbody id="peternakan-c-body">
+
+        </tbody>
+        </form>
+
+    </table>
+</div>
+<nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+    aria-label="Table navigation">
+    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+        Showing
+        <span id="peternakan-c-showing" class="font-semibold text-gray-900 dark:text-white">?</span>
+        of
+        <span id="peternakan-c-total" class="font-semibold text-gray-900 dark:text-white">?</span>
+    </span>
+    <ul id="peternakan-c-pagination" class="inline-flex items-stretch -space-x-px">
+
+    </ul>
+</nav>
+{{-- Tabel D --}}
+<div class="overflow-x-auto mt-4">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-4 py-3">
+                    <span class="sr-only">Actions</span>
+                </th>
+                <th scope="col" class="px-4 py-3 text-center">id</th>
+                <th scope="col" class="px-4 py-3 text-center">r310</th>
+                <th scope="col" class="px-4 py-3 text-center">r311</th>
+                <th scope="col" class="px-4 py-3 text-center">r312</th>
+                <th scope="col" class="px-4 py-3 text-center">r313</th>
+                <th scope="col" class="px-4 py-3 text-center">r314</th>
+                <th scope="col" class="px-4 py-3 text-center">r315</th>
+                <th scope="col" class="px-4 py-3 text-center">r316</th>
+                <th scope="col" class="px-4 py-3 text-center">r317</th>
+                <th scope="col" class="px-4 py-3 text-center">r318</th>
+                <th scope="col" class="px-4 py-3 text-center">r319</th>
+                <th scope="col" class="px-4 py-3 text-center">r320</th>
+                <th scope="col" class="px-4 py-3 text-center">r321</th>
+                <th scope="col" class="px-4 py-3 text-center">r322</th>
+                <th scope="col" class="px-4 py-3 text-center">r323</th>
+                <th scope="col" class="px-4 py-3 text-center">prov</th>
+                <th scope="col" class="px-4 py-3 text-center">kabkot</th>
+                <th scope="col" class="px-4 py-3 text-center">kec</th>
+                <th scope="col" class="px-4 py-3 text-center">desa</th>
+            </tr>
+        </thead>
+        <tbody id="peternakan-d-body">
+
+        </tbody>
+        </form>
+
+    </table>
+</div>
+<nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+    aria-label="Table navigation">
+    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+        Showing
+        <span id="peternakan-d-showing" class="font-semibold text-gray-900 dark:text-white">?</span>
+        of
+        <span id="peternakan-d-total" class="font-semibold text-gray-900 dark:text-white">?</span>
+    </span>
+    <ul id="peternakan-d-pagination" class="inline-flex items-stretch -space-x-px">
+
+    </ul>
+</nav>
