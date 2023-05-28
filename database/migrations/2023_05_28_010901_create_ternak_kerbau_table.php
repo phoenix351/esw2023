bd63591692f20f2b175266f590e0cd077e0b1217
Rifqi Mubarak <ryuuzumaru@gmail.com>
2023-05-28 10:30:10 +0800

    Update 2023_05_28_010901_create_ternak_kerbau_table.php

--- database/migrations/2023_05_28_010901_create_ternak_kerbau_table.php
+++ database/migrations/2023_05_28_010901_create_ternak_kerbau_table.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('ternak_kerbau', function (Blueprint $table) {
             $table->id();
+            $table->string('r602',20);
+            $table->string('r603',20);
+            $table->string('r604',20);
+            $table->string('r605',20);
+            $table->string('r606',20);
+            $table->string('r607',20);
+            $table->string('r608',20);
+            $table->string('r609',20);
+            $table->string('r610',20);
+            $table->string('r611',20);
+            $table->string('r612',20);
+            $table->string('r613',20);
+            $table->string('r617',20);
+            $table->string('r618',20);
             $table->timestamps();
         });
     }
